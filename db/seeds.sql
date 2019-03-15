f9d44f253af13e6459b7e1cfaa4028565e1c56e3
Techman <wizmar.tyler@gmail.com>
2019-03-14 22:56:37 -0500

    Finishing up program.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Ham');
+INSERT INTO burgers (name) VALUES ('Peanut Butter');
+INSERT INTO burgers (name, eaten) VALUES ('Cheese', true);
+INSERT INTO burgers (name, eaten) VALUES ('Turkey', true);
+INSERT INTO burgers (name, eaten) VALUES ('Egg', true);
+INSERT INTO burgers (name) VALUES ('Jelly');
